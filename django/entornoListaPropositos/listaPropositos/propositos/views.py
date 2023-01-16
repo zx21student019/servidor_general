4ed705ad6ac0c4165e4c6be0ea0beaa05858b2e5
more <zx21studentrcolegiovalledelmiro.es>
2023-01-16 13:35:09 +0100

    inicio de listaPropositos

---
+++ django/entornoListaPropositos/listaPropositos/propositos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Proposito
+
+def listadoPropositos(request):
+    propositos = Proposito.objects.all()
+    contexto = {
+        "propositos": propositos
+    }
+    return render(request,"propositos.html",contexto)
+
+def anadirPropositos(request):
+    nuevoProposito=Proposito(proposito=,)
