f064aa195bf489cd293dd426c850eb5240ce4e60
more <zx21studentrcolegiovalledelmiro.es>
2023-01-26 13:57:51 +0100

    snsa

--- django/entornoRecetarioV2/proyectoRecetario/recetario/admin.py
+++ django/entornoRecetarioV2/proyectoRecetario/recetario/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Receta, Categorias
 
+class RecetaAdmin(admin.ModelAdmin):
+    readonly_fields=('crecated','updated')
+    list_display=('nombre','subnombre','author')
+    ordering=('nombre','author')
+    search_fields=('nombre',)
+
 admin.site.register(Receta)
 admin.site.register(Categorias)
