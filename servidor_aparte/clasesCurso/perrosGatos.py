e7eed49d9fbcbcfd14b87cf6f5559c758699c284
zx21student019 <zx21student019@colegiovalledelmiro.es>
2023-02-04 16:10:13 +0100

    d

---
+++ servidor_aparte/clasesCurso/perrosGatos.py
@@ -0,0 +1,13 @@
+#!C:\Users\mores\AppData\Local\Microsoft\WindowsApps\python
+
+#class Gato:
+#    def __init__(self, nombre, onomatopeya):
+#        self.nombre = nombre
+#        self.onomatopeya = onomatopeya
+#
+#    def saludo(self):
+#        print('Hola mi nombre es ',self.nombre)
+#
+#gato = Gato('rudolf', 'miauuu')
+#gato.saludo()
+print ("hola")
