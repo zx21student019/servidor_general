23adba150cf169a4413cb03b110b9581ff5c540e
more <zx21studentrcolegiovalledelmiro.es>
2023-02-07 13:53:10 +0100

    ventaCochesBD

--- django/entornoVentaCoches/proyectoVentaCoches/proyectoVentaCoches/settings.py
+++ django/entornoVentaCoches/proyectoVentaCoches/proyectoVentaCoches/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'proyectoVentaCoches.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ventaCoches',
+        'USER': 'ventaCoches',
+        'PASSWORD': 'ventaCoches',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
